10ddcfebf8e1d7e1659d1783acf7b48c783b8dce
Simon <schmsimo@pdz-HP-Z1-Workstation.(none)>
2013-03-24 19:36:18 +0100

    updated init script

--- init.bash
+++ init.bash
@@ -1,5 +1,15 @@
 #!/bin/bash
-BASEDIR=$(dirname $0)
+BASEDIR=`pwd`
 
-catkin_init_workspace src/
-wstool init $BASEDIR/src /opt/ros/groovy
+source /opt/ros/groovy/setup.sh
+
+cd $BASEDIR/catkin_ws/src
+catkin_init_workspace
+cd $BASEDIR/catkin_ws
+catkin_make
+
+source devel/setup.bash
+
+cd $BASEDIR/rosbuild_ws
+rosws init . $BASEDIR/catkin_ws/devel
+
